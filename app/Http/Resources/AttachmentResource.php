73d2723e581ac3f3f5e939fe5638719fec0201e4
Hong10 <alexeyhong10@gmail.com>
2020-07-25 22:29:47 +0500

    Resources classes updated.
    - Updated
      PostResource
    - Added
      UserResource, CategoryResource

---
+++ app/Http/Resources/AttachmentResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AttachmentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        $data = parent::toArray($request);
+
+        unset($data['attachable_type']);
+        unset($data['attachable_']);
+
+        return $data;
+    }
+}
---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'order' => $this->order,
+            'attachments' => new AttachmentResource($this->whenLoaded('attachments')),
+            'user' => new UserResource($this->whenLoaded('user')),
+            'category' => new CategoryResource($this->whenLoaded('category')),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
+    }
+}
