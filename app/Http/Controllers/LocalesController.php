27907cf42f861d93a1a6154a27a9fb3f86d75830
Hong10 <alexeyhong10@gmail.com>
2020-12-05 00:10:28 +0500

    Added translations/locales into file cache.

--- app/Http/Controllers/LocalesController.php
+++ app/Http/Controllers/LocalesController.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Cache;
+
 class LocalesController extends Controller
 {
     public function index()
     {
-        return response()->json([
+        if ($locales = Cache::get('locales')) {
+            return response()->json($locales);
+        }
+
+        $locales = [
             'locale' => app()->getLocale(),
             'languages' => config('app.supported_locales'),
-        ]);
+        ];
+
+        Cache::put('locales', $locales);
+
+        return response()->json($locales);
     }
 }
--- app/Http/Controllers/TranslationsController.php
+++ app/Http/Controllers/TranslationsController.php
@@ -2,16 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Cache;
+
 class TranslationsController extends Controller
 {
     public function index()
     {
-        return response()->json([
+        if ($translations = Cache::get('translations')) {
+            return response()->json($translations);
+        }
+
+        $translations = [
             'admin_panel' => trans('admin_panel'),
             'auth' => trans('auth'),
             'pagination' => trans('pagination'),
             'passwords' => trans('passwords'),
             'validation' => trans('validation'),
-        ]);
+        ];
+
+        Cache::put('translations', $translations);
+
+        return response()->json($translations);
     }
 }
