5dd8f6bbec0b989f60c30babf4036cefc7a5bbe7
Hong10 <alexeyhong10@gmail.com>
2020-12-10 23:00:47 +0500

    Make SetLocale middleware globally.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -26,7 +26,6 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            SetUserLocale::class
         ],
     ];
 
@@ -45,6 +44,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+        SetUserLocale::class
     ];
 
     /**
